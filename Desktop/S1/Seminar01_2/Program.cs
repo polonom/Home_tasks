df42fd9912bb1f9d63bb4d06749b206ce58d0ea7
Polo_a <kiviku7@mail.ru>
2022-08-06 00:22:31 +0300

    Zadanie_2

---
+++ Desktop/S1/Seminar01_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (max<numberB) max=numberB;
+if(max<numberC) {max = numberC;}
+Console.WriteLine(max);
+
