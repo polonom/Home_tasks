6a10f33a50363c3550e0461887098c3de2bd9b38
Polo_a <kiviku7@mail.ru>
2022-08-14 10:10:07 +0300

    Task3 seminar2

---
+++ Desktop/S1/HW2/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+bool IsItWeekend(int number){
+    if (number == 6 | number == 7){
+        return true;
+}
+     else{
+        return false;
+     }
+}
+var input = Convert.ToInt32(Console.ReadLine());
+ bool result = IsItWeekend(input);
+ if (result == true){
+    Console.WriteLine("Да, это выходной");
+ }
+ else{
+     Console.WriteLine("Увы!");
+ }
