c7febfc4d791b8016309e895fef58cdceb63aa7a
Polo_a <kiviku7@mail.ru>
2022-08-14 10:06:59 +0300

    Task1 seminar2

---
+++ Desktop/S1/HW2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int ShowMeSecondNumber(int number)
+{
+int n = (number/10)%10;
+Console.WriteLine($"Второе число - {n}");
+return n;
+}
+var input = Convert.ToInt32(Console.ReadLine());
+if (input>999|input<100){
+    Console.WriteLine("Введите трехзначное число!");
+}
+else{
+    var n = ShowMeSecondNumber(input);
+}
