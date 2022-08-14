38ba601b575d3ac1b6665df9507b5bc02e1a7bd3
Polo_a <kiviku7@mail.ru>
2022-08-14 10:08:56 +0300

    Task2 seminar2

---
+++ Desktop/S1/HW2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int? ShowMeNumberThree(int number){
+if (number<100){
+    Console.WriteLine("Третьей цифры нет!");
+    return null;
+}
+while (number>999){
+   number = number/10;
+}
+int m = number % 10;
+Console.WriteLine($"Третья цифра - {m}");
+return m;
+}
+var input = Convert.ToInt32(Console.ReadLine());
+ var k = ShowMeNumberThree(input);
+ 
+
