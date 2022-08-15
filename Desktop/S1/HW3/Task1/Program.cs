27c849e3364bb1a6c6cf97615b5cfc2f3615fe67
Polo_a <kiviku7@mail.ru>
2022-08-15 10:45:02 +0300

    pro palidrom

---
+++ Desktop/S1/HW3/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+bool IsItPolindrom(string smt){
+for (int i = 0 ; i < smt.Length/2 ; i++) {
+    if (smt[i] != smt[smt.Length-i-1]) {
+        return false;
+    }
+    else continue;
+}
+return true;
+}
+string input = Console.ReadLine();
+var res = IsItPolindrom(input);
+if (res == true) {
+    Console.WriteLine($"{input}-палиндром");
+}
+else  Console.WriteLine($"{input}- не палиндром");
