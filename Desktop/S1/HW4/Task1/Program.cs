1d96dee1f0f0ad9587fd3ecd894037a4f62f9d58
Polo_a <kiviku7@mail.ru>
2022-08-17 23:40:35 +0300

    t1

---
+++ Desktop/S1/HW4/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+double GiveMePower(int A, int B)
+{
+    double C = Math.Pow(A, B);
+    return C;
+}
+Console.WriteLine("Какое число хотите возвести?");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень?");
+int pow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GiveMePower(num, pow));
