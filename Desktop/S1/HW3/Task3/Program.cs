6aeeec59fa0046a33a0ef2b9110dfb9a9199b928
Polo_a <kiviku7@mail.ru>
2022-08-17 21:36:36 +0300

    task3

--- Desktop/S1/HW3/Task3/Program.cs
+++ Desktop/S1/HW3/Task3/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+string GiveMeCube(int number){
+    string res = "";
+    for (int i = 1; i<=number; i++){
+     res += $"{Math.Pow(i,3)}  ";
+    }
+    return res;
+}
+var input = Convert.ToInt32(Console.ReadLine());
+var p = GiveMeCube(input);
+Console.WriteLine(p);
