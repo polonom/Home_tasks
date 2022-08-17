8467a45462702751cf8df623f36ec43b801fe891
Polo_a <kiviku7@mail.ru>
2022-08-18 00:42:00 +0300

    t3

--- Desktop/S1/HW4/Task3/Program.cs
+++ Desktop/S1/HW4/Task3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] mas = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+string s = string.Join(",", mas);
+Console.Write($"[{s}]");
