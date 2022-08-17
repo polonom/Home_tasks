33947d33fa24c8468a6df91a984cc6b0c4bb59a6
Polo_a <kiviku7@mail.ru>
2022-08-18 00:02:26 +0300

    t2

--- Desktop/S1/HW4/Task2/Program.cs
+++ Desktop/S1/HW4/Task2/Program.cs
@@ -1,1 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int GiveMeSum(int number)
+{
+    int result = 0;
+    result = number % 10;
+    while (number >= 10)
+    {
+        number = number / 10;
+        result += number % 10;
+    }
+
+    return result;
+}
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумму цифр в числе = {GiveMeSum(N)}");
