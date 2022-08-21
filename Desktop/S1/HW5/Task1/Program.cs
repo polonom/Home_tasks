423f2ef120fc13e99e8ce02179543921688a3c0c
Polo_a <kiviku7@mail.ru>
2022-08-21 20:17:53 +0300

    t2

---
+++ Desktop/S1/HW5/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] arr = new int[4];
+int[] FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(100, 1000);
+    }
+    return array;
+}
+
+int FindEven(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+}
+
+int[] randArray = FillArray(arr);
+int res = FindEven(randArray);
+PrintArray(randArray);
+Console.WriteLine($"Число четных чисел  - {res}");
---
+++ Desktop/S1/HW5/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] arr= new int[4];
+int[] FillArray(int[] array){
+    Random random = new Random();
+    for (int i=0 ; i<array.Length; i++){
+        array[i] = random.Next(1,100);
+    }
+return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+}
+int FindSummOfIndex(int[] array){
+    int summ=0;
+    for (int i = 1; i<array.Length; i=i+2){
+        summ=summ+array[i];
+    }
+    return summ;
+}
+int[] randArray = FillArray(arr);
+PrintArray(randArray);
+int res = FindSummOfIndex(randArray);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях - {res}");
