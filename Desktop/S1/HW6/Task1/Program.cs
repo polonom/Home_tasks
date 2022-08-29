710457c7e91de2605eec47f6870375956c68cea2
Polo_a <kiviku7@mail.ru>
2022-08-29 23:24:29 +0300

    task1

---
+++ Desktop/S1/HW6/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int CountNumbersGraterThanZero(int[] array){
+    int counter = 0;
+    for (int i = 0; i< array.Length; i++){
+        if (array[i] > 0){
+            counter++;
+        }
+    }
+    return counter;
+}
+
+Console.WriteLine("Введите набор чисел");
+string input = Console.ReadLine();
+int[] GiveMeAnArray(string line){
+   int[] arr = line.Split(',').Select(int.Parse).ToArray();
+   return arr;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"  {array[i]} ");
+    }
+}
+PrintArray(GiveMeAnArray(input));
+int[] newarr = GiveMeAnArray(input);
+int res = CountNumbersGraterThanZero(newarr);
+Console.WriteLine($"Количество положительных чисел в строке - {res}");
