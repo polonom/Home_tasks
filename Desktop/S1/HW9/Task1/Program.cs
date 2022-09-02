4b38032d4380e4b2f0db6e11adbbf6414025bec3
Polo_a <kiviku7@mail.ru>
2022-09-03 00:52:37 +0300

    task1

---
+++ Desktop/S1/HW9/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число");
+bool isNumberM = int.TryParse(Console.ReadLine(),out int m);
+Console.WriteLine("Введите второе число");
+bool isNumberN = int.TryParse(Console.ReadLine(),out int n);
+if (isNumberM == false || isNumberN == false){
+    Console.WriteLine("Введены не правельные данные");
+    return;
+}
+void PrintNumbers(int n,int m){
+    if (n<m){
+        Console.Write($" {n} ");
+    if (n==m) return;
+    PrintNumbers(n+1,m);
+    }
+    else{
+        Console.Write($" {m} ");
+    if (n==m) return;
+    PrintNumbers(n,m+1);
+    }
+    
+}
+PrintNumbers(n,m);
