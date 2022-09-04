048f08376603aefa5a217d8cd60af1373c9e275b
Polo_a <kiviku7@mail.ru>
2022-09-04 12:31:56 +0300

    task5

---
+++ Desktop/S1/HW8/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Задайте первое число");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Задайте второе число");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+Console.WriteLine("Задайте третье число");
+bool isNumberF = int.TryParse(Console.ReadLine(), out int f);
+
+if (isNumberM == false || isNumberN == false || isNumberF==false)
+{
+    Console.WriteLine("Введены не правельные данные");
+    return;
+}
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
+
+int[,,] array = new int[m,n,f];
---
+++ Desktop/S1/HW8/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+
+    }
+}
+int[,] MakeArray()
+{
+    int[,] array = new int[4, 4];
+    int count = 1;
+    int i = 0;
+    int j = 0;
+    while (count <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = count;
+        count++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return array;
+}
+int[,] res = MakeArray();
+PrintArray2D(res);
